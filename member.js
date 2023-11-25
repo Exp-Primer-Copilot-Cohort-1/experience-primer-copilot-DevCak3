a5a16ec4dda0da51be6f1836bb5c23c23d194671
Camilo Andres Rosero Hernandez <144865123+DevCak3@users.noreply.github.com>
2023-11-25 02:35:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberButton = document.getElementById("memberButton");
+  var skillsButton = document.getElementById("skillsButton");
+  var memberButton2 = document.getElementById("memberButton2");
+  var skillsButton2 = document.getElementById("skillsButton2");
+  member.style.display = "block";
+  skills.style.display = "none";
+  memberButton.style.backgroundColor = "#4CAF50";
+  skillsButton.style.backgroundColor = "#2ECC71";
+  memberButton2.style.backgroundColor = "#4CAF50";
+  skillsButton2.style.backgroundColor = "#2ECC71";
+}
